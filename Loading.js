d8cac343701921841e6d2c61697c5a8469ecdb41
harmakit <harmakit@gmail.com>
2018-03-02 12:45:31 +0700

    page loading exapmle

---
+++ Loading.js
@@ -0,0 +1,18 @@
+var Status = require('./Status');
+
+var pageLoadingStatus = new Status();
+pageLoadingStatus.subscribe(function() {console.log("Loading")});
+
+var pageLoading = {
+  chunks: 0,
+  loadChunk: function(chunks) {
+    chunks++;
+    if((chunks % 10) == 0)
+      console.log("Loaded " + chunks + " chunks!")
+  }
+}
+
+pageLoadingStatus.subscribe(pageLoading.loadChunk, pageLoading);
+
+for (var i = 1; i < 100; i++)
+  pageLoadingStatus.raise(i);
